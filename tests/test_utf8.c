7905faf924fba1407d9ef6ca275aeb2c6093407a
Duong H. Nguyen <cmpitg@gmail.com>
2013-11-07 14:28:09 +0700

    Suspend some tests, focus on counting bytes

--- tests/test_utf8.c
+++ tests/test_utf8.c
@@ -29,6 +29,30 @@
 bgstr   mainString, buff;
 bglen_t i;
 
+int testCountBytes (void) {
+    initTestCase ("bgcharCountBytes | bgstrCountBytes");
+
+    const char ch1[6] = "à";
+    const char ch2[6] = "ế";
+    const char ch3[6] = "ư";
+    const char ch4[6] = "ờ";
+    const char str[24] = "ường";
+
+    const int len1 = strlen_ (ch1);    /* 2 */
+    const int len2 = strlen_ (ch2);    /* 3 */
+    const int len3 = strlen_ (ch3);    /* 2 */
+    const int len4 = strlen_ (ch4);    /* 3 */
+    const int lenStr = strlen_ (str);  /* 7 */
+
+    assertInt (2, bgcharCountBytes (ch1));
+    assertInt (3, bgcharCountBytes (ch2));
+    assertInt (2, bgcharCountBytes (ch3));
+    assertInt (3, bgcharCountBytes (ch4));
+    assertInt (2, bgcharCountBytes (str));
+
+    return finishTestCase ();
+}
+
 int testLengthFunctions (void) {
     initTestCase ("bgcharLen | bgstrLen >> input: utf8_input.txt");
 
@@ -104,40 +128,42 @@ int testStringDuplication (void) {
 int testStringCopy (void) {
     initTestCase ("bgstrCopy | bgstrAssign");
 
-    bgstr str1, str2, str3;
+    bgstr str1, str2, str3, str4;
     char pointerAddr1[20];
     char pointerAddr2[20];
     char pointerAddr3[20];
 
     strToBgstr ("Xin chào thế giới!", str1);
-    bgstrCopy (str1, str2);
+    bgstrCopy (str1, str2, 0, bgstrLen (str1));
     bgstrAssign (str3, str2);
+    bgstrCopy (str1, str4, 0, 7);
 
     assertStr (str1, str2);
     assertStr (str1, str3);
+    assertStr ("Xin chà", str4);
 
     return finishTestCase ();
 }
 
-int testInsertChar (void) {
-    initTestCase ("bgstrInsertCharAt");
+/* int testInsertChar (void) { */
+/*     initTestCase ("bgstrInsertCharAt"); */
 
-    bgstr aString;
-    bgstr aChar;
+/*     bgstr aString; */
+/*     bgstr aChar; */
 
-    strToBgstr ("Xin chào t", aString);
+/*     strToBgstr ("Xin chào t", aString); */
 
-    bgstrInsertCharAt (aString, aString, " ", 0);
-    assertStr (" Xin chào t", aString);
+/*     bgstrInsertCharAt (aString, aString, " ", 0); */
+/*     assertStr (" Xin chào t", aString); */
 
-    bgstrInsertCharAt (aString, aString, "h", 11);
-    assertStr (" Xin chào th", aString);
+/*     bgstrInsertCharAt (aString, aString, "h", 11); */
+/*     assertStr (" Xin chào th", aString); */
 
-    bgstrInsertCharAt (aString, aString, "ế", 12);
-    assertStr (" Xin chào thế", aString);
+/*     bgstrInsertCharAt (aString, aString, "ế", 12); */
+/*     assertStr (" Xin chào thế", aString); */
 
-    return finishTestCase ();
-}
+/*     return finishTestCase (); */
+/* } */
 
 void readInput (void) {
     /* This is supposed to read from `test_utf8_input.txt` */
@@ -149,12 +175,14 @@ int main (int argc, char *argv[]) {
     readInput ();
     printf ("\n");
 
+    addTest (testCountBytes);
+
     addTest (testLengthFunctions);
     addTest (testCharAccess);
     addTest (testStringConversion);
     addTest (testStringDuplication);
-    addTest (testStringCopy);
-    addTest (testInsertChar);
+    /* addTest (testStringCopy); */
+    /* addTest (testInsertChar); */
 
     runAllTests ();
 
